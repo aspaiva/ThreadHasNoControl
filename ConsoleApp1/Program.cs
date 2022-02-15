d6ce6414fa5332040cfbb4b4f318f6b08af3828f
Alexandre Soares Paiva <alexandrepaiva@br.com.br>
2022-02-15 00:50:45 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+static void DoSomething(string What, int HowLong)
+{
+    Console.WriteLine($"Inicio: {What}");
+    Thread.Sleep(HowLong);
+    Console.WriteLine($"Fim: {What}");
+}
+
+while (true)
+{
+    new Thread(new ThreadStart(() => DoSomething("1", 2000))).Start();
+    new Thread(new ThreadStart(() => DoSomething("2", 2000))).Start();
+    new Thread(new ThreadStart(() => DoSomething("3", 2000))).Start();
+
+    var key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Enter)
+    {
+        break;
+    }
+    Console.WriteLine("de novo...");
+}
